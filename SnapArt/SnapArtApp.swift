21163366cc845122c43720d6ae55e477c7ca9610
GarryGao00 <57919239+GarryGao00@users.noreply.github.com>
2025-01-22 22:24:41 -0800

    Initial Commit

---
+++ SnapArt/SnapArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapArtApp.swift
+//  SnapArt
+//
+//  Created by Garry Mackinaw on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapArtTests/SnapArtTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapArtTests.swift
+//  SnapArtTests
+//
+//  Created by Garry Mackinaw on 1/22/25.
+//
+
+import Testing
+@testable import SnapArt
+
+struct SnapArtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
